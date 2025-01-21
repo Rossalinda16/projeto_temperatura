8e72ef39e4048d3025f06369c03e5043448974cb
Rosa Linda <rossalindaperez16@gmail.com>
2025-01-21 01:00:08 -0300

    Realizando testes

--- escala_termometrica.go
+++ escala_termometrica.go
@@ -4,9 +4,9 @@ import "fmt"
 
 func main() {
 
-	const ebulicao_K = 373
+	const ebulicao_K = 373.15
 
-	celsius := ebulicao_K - 273
-	fmt.Printf("O ponto de ebulição da água é %d K, que corresponde a %d °C.", ebulicao_K, celsius)
+	celsius := ebulicao_K - 273.15
+	fmt.Printf("O ponto de ebulição da água é %f K, que corresponde a %f °C.", ebulicao_K, celsius)
 
 }
