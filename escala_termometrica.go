f86c18af74d65dc35ea310801ee4d91124ee1d71
Rosa Linda Perez🍃 <158646679+Rossalinda16@users.noreply.github.com>
2025-01-22 20:22:59 -0300

    Update escala_termometrica.go

--- escala_termometrica.go
+++ escala_termometrica.go
@@ -4,9 +4,9 @@ import "fmt"
 
 func main() {
 
-	const ebulicao_K = 373.15
+	const ebulicao_K = 373
 
-	celsius := ebulicao_K - 273.15
+	celsius := ebulicao_K - 273
 	fmt.Printf("O ponto de ebulição da água é %f K, que corresponde a %f °C.", ebulicao_K, celsius)
 
 }
